5ca118b9067bcecf8608ec62a7c41204ac246c80
Vo Minh Tri <trivm2472@gmail.com>
2022-07-12 17:01:31 +0700

    add class format

--- back-end/src/controllers/authentication.controller.ts
+++ back-end/src/controllers/authentication.controller.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+var validator = require('validator');
 
 class Auth {
 
+  public validate (fullName: string, email: string, userName: string, password: string) {
+    // if (!validator.isEmail(email)) {
+    //   return {success: false, error: 'Email must be in correct format'};
+    // }
+    // return {success: true};
+    var obj = {success: true};
+    return obj;
+  }
+
   public async register (req: express.Request, res: express.Response) {
-    res.json({message: "login success"});
+    var {fullName, email, userName, password} = req.body; 
+    
+    res.json({success: true});
+    
   }
 
 }
-export {Auth};
+module.exports = {Auth};
