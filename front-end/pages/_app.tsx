9e7f67661b74b27288a7053b10af97289ea46b85
vuhah <vuuu0412@gmail.com>
2022-07-13 15:08:03 +0700

    updated by vu

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,8 +1,8 @@
-// import 'styles/globals.css'
 import "styles/index.scss";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   useEffect(() => {
