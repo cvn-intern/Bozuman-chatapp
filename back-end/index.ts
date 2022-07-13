4e09f99c6267d7b70304ff6baffe51f00ac391fc
Vo Minh Tri <trivm2472@gmail.com>
2022-07-13 11:16:46 +0700

    finish init back-end

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,19 +2,13 @@ import express, { Application, Request, Response } from "express";
 const auth = require('./src/routes/authentication.route');
 const app: Application = express();
 const port = 3000;
+const { Database } = require('./src/configs/db.config');
 
+const db = new Database();
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get(
-  "/",
-  async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).send({
-      message: "Hello World!",
-    });
-  }
-);
 
 app.use('/api/auth',auth);
 
---
+++ back-end/src/models/users.model.ts
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const UsersSchema = Schema({
+
+    username:{
+        type: String,
+        required: true,
+    },
+
+    password:{
+        type: String,
+        required: true,
+    },
+
+    full_name: {
+        type: String,
+        required: true,
+    },
+
+    email:{
+        type: String,
+        required: true,
+    },
+
+    active:{
+        type: Boolean,
+        required: false,
+    },
+
+    birth_day:{
+        type: Date,
+        required: false,
+    },
+
+    gender: {
+        type: Boolean,
+        required: false,
+    },
+
+    phone:{
+        type: String,
+        required: false,
+    },
+
+    avatar:{
+        type: String,
+        required: false,
+    },
+
+    description:{
+      type: String,
+      required: false,
+    },
+
+    room_list: {
+      type: Array,
+      required: false,
+    },
+
+});
+
+
+module.exports = mongoose.model("Users", UsersSchema);
