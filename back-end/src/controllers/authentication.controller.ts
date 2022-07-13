3f3cf4b6738b80d94fb6c8c5d25f39bc3297d8b1
Vo Minh Tri <trivm2472@gmail.com>
2022-07-13 08:16:37 +0700

    finish init back-end

--- back-end/src/controllers/authentication.controller.ts
+++ back-end/src/controllers/authentication.controller.ts
@@ -1,21 +1,26 @@
 import express from 'express';
 var validator = require('validator');
-
+const { AuthModel } = require('../models/authentication.model')
 class Auth {
 
-  public validate (fullName: string, email: string, userName: string, password: string) {
+  private model: any;
+
+  constructor() {
+    this.model = new AuthModel();
+  }
+
+  public validate  = () => {
     // if (!validator.isEmail(email)) {
     //   return {success: false, error: 'Email must be in correct format'};
     // }
     // return {success: true};
-    var obj = {success: true};
-    return obj;
+    return 'abc';
   }
 
-  public async register (req: express.Request, res: express.Response) {
+  public register = async (req: express.Request, res: express.Response) => {
     var {fullName, email, userName, password} = req.body; 
-    
-    res.json({success: true});
+    var result = this.model.register();
+    res.json(result);
     
   }
 
---
+++ back-end/src/models/authentication.model.ts
@@ -0,0 +1,12 @@
+class AuthModel {
+
+  constructor() {
+    
+  }
+  public register = () => {
+    return {message: "register complete"};
+  }
+
+}
+
+module.exports = {AuthModel};
