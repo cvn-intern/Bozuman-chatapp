19241444995d0c00eb9317756cf761cd507aaf67
caohaisil201 <caohaisil@gmail.com>
2022-07-18 16:02:14 +0700

    [Forgot-password] delete dist folder

--- back-end/dist/src/configs/auth.config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const config = Object.freeze({
-    SECRET: 'SECRET_BOZUMAN',
-    SECRET_REFRESH: 'SECRET_REFRESH_BOZUMAN',
-    tokenLife: 60,
-    refreshTokenLife: 300
-});
-module.exports = config;
