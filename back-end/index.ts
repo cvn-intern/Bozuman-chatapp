0dff654b88408f182a02233aff06034b8c2d84ae
caohaisil201 <caohaisil@gmail.com>
2022-07-25 11:25:40 +0700

    [hotfix/deploy] delete index from master

--- back-end/index.ts
+++ back-end/index.ts
@@ -1,78 +1,45 @@
 
-// import { Database } from './src/configs/db.config';
-// import { expiredAccessTokenHandler } from './src/middlewares/expiredAccessTokenHandler';
-// import {checkAccessToken} from './src/middlewares/checkAccessToken';
-// import express, { Application } from 'express';
-// import auth from './src/routes/authentication.route';
-// import cors from 'cors';
-
-// const timeout = require('connect-timeout');
-
-// const db = new Database();
-// // eslint-disable-next-line @typescript-eslint/no-floating-promises
-// db.dbConnect();
-// const app: Application = express();
-
-// app.use(timeout('3600s'));
-
-// // Body parsing Middleware
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   cors({
-//     origin: [
-//       'http://127.0.0.1:3001',
-//       'http://localhost:3001',
-//       'https://bozuman-chatapp-staging.vercel.app',
-//       'https://bozuman-chatapp.vercel.app',
-//     ],
-//     credentials: true,
-//   })
-// );
-
-// app.use('/api/auth', auth);
-// app.use('/api/token', expiredAccessTokenHandler);
-// app.use(checkAccessToken);
-// // secure route goes here
-// app.use('/', (req, res) => {
-//   res.json({ success: 'ok' });
-// });
-// const port = process.env.PORT || 3000;
-// app.listen(port, (): void => {
-//   /* eslint-disable no-debugger, no-console */
-//   console.log(`Connected successfully on port ${port}`);
-// });
-
-const expiredAccessTokenHandler = require('./src/middlewares/expiredAccessTokenHandler')
-const { Database } = require('./src/configs/db.config');
-
+import { Database } from './src/configs/db.config';
+import { expiredAccessTokenHandler } from './src/middlewares/expiredAccessTokenHandler';
+import {checkAccessToken} from './src/middlewares/checkAccessToken';
 import express, { Application } from 'express';
 import auth from './src/routes/authentication.route';
 import cors from 'cors';
+
+const timeout = require('connect-timeout');
+
 const db = new Database();
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+db.dbConnect();
 const app: Application = express();
+
+app.use(timeout('3600s'));
 
 // Body parsing Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: ['http://127.0.0.1:3001','http://localhost:3001', 'https://bozuman-chatapp-staging.vercel.app', 'https://bozuman-chatapp.vercel.app'],
-    credentials: true, //Để bật cookie HTTP qua CORS // Chỉ có thể set cookie trên cùng domain
-    // methods: 'POST',
-    // maxAge: 84000
+    origin: [
+      'http://127.0.0.1:3001',
+      'http://localhost:3001',
+      'https://bozuman-chatapp-staging.vercel.app',
+      'https://bozuman-chatapp.vercel.app',
+    ],
+    credentials: true,
   })
 );
 
 app.use('/api/auth', auth);
-app.use('/api/token', expiredAccessTokenHandler)
-app.use(require('./src/middlewares/checkAccessToken'))
+app.use('/api/token', expiredAccessTokenHandler);
+app.use(checkAccessToken);
 // secure route goes here
-app.use('/', (req, res)=>{
-  res.json({success:'ok'})
-})
+app.use('/', (req, res) => {
+  res.json({ success: 'ok' });
+});
 const port = process.env.PORT || 3000;
 app.listen(port, (): void => {
   /* eslint-disable no-debugger, no-console */
   console.log(`Connected successfully on port ${port}`);
 });
+
